24252cd9fea942939b28c88bad70e6bb9fb5d42f
Guillaume Delamotte <guillaume.delamotte@hotmail.fr>
2021-12-01 14:30:35 +0100

    Front-end Game form

--- server/app/Views/Home/index.php
+++ server/app/Views/Home/index.php
@@ -6,36 +6,27 @@
 
     <?php include('../layout/header.php'); ?>
 
-    <main>
-        <div class="py-5 text-center">
-            <img class="d-block mx-auto mb-4" src="/docs/5.1/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
-            <h2>Game form</h2>
-            <p class="lead">Project NoSQL testing form</p>
-        </div>
-        <div class="row g-5">
-            <div class="col-md-7 col-lg-8">
-                <h4 class="mb-3">Add a game</h4>
-                <form class="needs-validation" novalidate>
-                    <div class="row g-3">
-                        <div class="col-sm-6">
-                            <label for="firstName" class="form-label">Game name</label>
-                            <input type="text" class="form-control" id="gameName" name="gameName" placeholder="" value="" required>
-                            <div class="invalid-feedback">
-                                Valid name is required.
-                            </div>
-                        </div>
-
-                        <div class="col-sm-6">
-                            <label for="lastName" class="form-label">Last name</label>
-                            <input type="text" class="form-control" id="description" name="description" placeholder="" value="" required>
-                            <div class="invalid-feedback">
-                                Valid description is required.
-                            </div>
-                        </div>
-                    </div>
-                </form>
+    <div class="container-sm">
+        <main class="form-signin">
+            <div class="py-5 text-center">
+                <img class="d-block mx-auto mb-4" src="../../Content/png/nosql-logo.png" alt="" width="72" height="72">
+                <h2>Project NoSQL</h2>
+                <p class="lead">Game testing form</p>
             </div>
-        </div>
-    </main>
+            <h4 class="mb-3">Add a game</h4>
+            <form class="needs-validation" novalidate>
+                <label for="name" class="form-label">Name</label>
+                <input type="text" class="form-control" id="name" name="name" placeholder="" value="" required>
+                <div class="invalid-feedback">
+                    Valid name is required.
+                </div>
+                <label for="description" class="form-label">Description</label>
+                <textarea class="form-control" id="description" name="description" rows="3" required></textarea>
+                <div class="invalid-feedback">
+                    Valid description is required.
+                </div>
+            </form>
+        </main>
+    </div>
 
     <?php include('../layout/footer.php'); ?>
