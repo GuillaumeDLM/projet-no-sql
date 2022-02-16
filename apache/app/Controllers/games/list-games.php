c59bc804a6321be60740bd057f23e516dc4ed1c6
Guillaume Delamotte <guillaume.delamotte@hotmail.fr>
2022-02-16 14:54:30 +0100

    indentation

--- apache/app/Controllers/games/list-games.php
+++ apache/app/Controllers/games/list-games.php
@@ -18,26 +18,24 @@
 		];
 
 		$response = $client->search($params);
-
-
-	$response = json_decode(json_encode($response),true);
-    $hits = count($response['hits']['hits']);
-    $result = null;
-    $i = 0;
-	
-    while ($i < $hits) {
-		$result[$i] = $response['hits']['hits'][$i]['_source'];
-		$i++;
-    }
-
-    foreach ($result as $key => $value) {
-		if(isset($value["name"])){
-			echo "<b>- " . $value['name'] . "</b>". ": ";
-			echo $value['description'] . "<br>";
+		$response = json_decode(json_encode($response),true);
+		$hits = count($response['hits']['hits']);
+		$result = null;
+		$i = 0;
+		
+		while ($i < $hits) {
+			$result[$i] = $response['hits']['hits'][$i]['_source'];
+			$i++;
 		}
-    }
 
-	$client = $deconnexionEs();
+		foreach ($result as $key => $value) {
+			if(isset($value["name"])){
+				echo "<b>- " . $value['name'] . "</b>". ": ";
+				echo $value['description'] . "<br>";
+			}
+		}
+
+		$client = $deconnexionEs();
 	} catch (\Exception $e) {
 		echo 'ERREUR : ', $e;
 	}
